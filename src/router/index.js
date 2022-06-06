2a10f2ec22f78e699b5f02a6341edd32e1271d70
Mohammad Yudha Pamungkas <93898512+yudhaph0300@users.noreply.github.com>
2022-06-06 18:40:07 +0700

    Create UI for Homepage

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import LoginView from '../views/Login.vue'
+import HomePage from '../views/HomePage.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'login',
     component: LoginView
+  },
+  {
+    path: '/homepage',
+    name: 'Home Page',
+    component: HomePage
   },
 ]
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,77 +1,79 @@
 <template>
-    <v-row>
-        <v-col class="pr-0 pb-0">
-            <v-img height="100%" src="../assets/login/background.png"/>
-        </v-col>
-        <v-col class="pl-0">
-            <v-card class="mx-12 my-16" flat>
-                <v-card-text class="text-center heading-1">
-                    Masuk
-                </v-card-text>
+    <v-app>
+        <v-row>
+            <v-col class="pr-0 pb-0">
+                <v-img height="100%" src="../assets/login/background.png"/>
+            </v-col>
+            <v-col class="pl-0">
+                <v-card class="mx-12 my-16" flat>
+                    <v-card-text class="text-center heading-1">
+                        Masuk
+                    </v-card-text>
 
-                <v-card-text class="pa-0 body-regular-2">
-                    Email
-                </v-card-text>
-                <v-text-field
-                 outlined
-                 placeholder="Masukan email anda"
-                 class="body-regular-3"
-                />
+                    <v-card-text class="pa-0 body-regular-2">
+                        Email
+                    </v-card-text>
+                    <v-text-field
+                    outlined
+                    placeholder="Masukan email anda"
+                    class="body-regular-3"
+                    />
 
-                <v-card-text class="pa-0 body-regular-2">
-                    Kata Sandi
-                </v-card-text>
-                <v-text-field
-                 outlined
-                 placeholder="Masukan kata sandi anda"
-                 class="body-regular-3"
-                 :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                 :type="showPassword ? 'text' : 'password'"
-                  @click:append="showPassword = !showPassword"
-                />
+                    <v-card-text class="pa-0 body-regular-2">
+                        Kata Sandi
+                    </v-card-text>
+                    <v-text-field
+                    outlined
+                    placeholder="Masukan kata sandi anda"
+                    class="body-regular-3"
+                    :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="showPassword ? 'text' : 'password'"
+                    @click:append="showPassword = !showPassword"
+                    />
 
-                <v-row>
-                    <v-col class="text-left py-0">
-                        <v-checkbox
-                        class="my-0 py-0"
-                        color="#121950"
-                        >
-                            <template v-slot:label>
-                                <div class="body-regular-3">
-                                    Simpan Kata Sandi
-                                </div>
-                            </template>
-                        </v-checkbox>
-                    </v-col>
-                    <v-col class="text-right py-0">
-                        <a class="body-regular-3 link-forger-pass text-decoration-none" href="#">Lupa kata sandi ?</a>
-                    </v-col>
-                </v-row>
+                    <v-row>
+                        <v-col class="text-left py-0">
+                            <v-checkbox
+                            class="my-0 py-0"
+                            color="#121950"
+                            >
+                                <template v-slot:label>
+                                    <div class="body-regular-3">
+                                        Simpan Kata Sandi
+                                    </div>
+                                </template>
+                            </v-checkbox>
+                        </v-col>
+                        <v-col class="text-right py-0">
+                            <a class="body-regular-3 link-forger-pass text-decoration-none" href="#">Lupa kata sandi ?</a>
+                        </v-col>
+                    </v-row>
 
-                <v-row>
-                    <v-col class="text-center">
-                        <v-btn
-                         color="#606FAA"
-                         dark
-                         class="px-10"
-                        >
-                            Masuk
-                        </v-btn>
-                    </v-col>
-                </v-row>
+                    <v-row>
+                        <v-col class="text-center">
+                            <v-btn
+                            color="#606FAA"
+                            dark
+                            class="px-10"
+                            >
+                                Masuk
+                            </v-btn>
+                        </v-col>
+                    </v-row>
 
-                <v-card-text class="text-center body-regular-2">
-                    atau
-                </v-card-text>
+                    <v-card-text class="text-center body-regular-2">
+                        atau
+                    </v-card-text>
 
-                <v-card-text class="text-center body-regular-2">
-                    Belum punya akun ? <a class="link text-decoration-none" href="/register">Daftar</a>
-                </v-card-text>
-                
-                
-            </v-card>
-        </v-col>
-    </v-row>
+                    <v-card-text class="text-center body-regular-2">
+                        Belum punya akun ? <a class="link text-decoration-none" href="/register">Daftar</a>
+                    </v-card-text>
+                    
+                    
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-app>
 </template>
 
 <script>
