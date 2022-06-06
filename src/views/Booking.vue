ec99df232dd8676087d6f9eefd04d0223846696a
John Doe <johndoe@email.com>
2022-06-06 10:11:58 +0700

    create booking.vue

---
+++ src/views/Booking.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="booking">
+    <div class="container">
+        <Navbar />
+    </div>
+    <div>
+        <h2>this is booking page</h2>
+    </div>
+</div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+    name: "Booking",
+    components: {
+        Navbar
+    }
+}
+</script>
