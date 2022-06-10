bb6461c48d3f2ecf84a1c4734f1d2177633575fd
miftahasiboean <miftaheczson027@gmail.com>
2022-06-10 17:02:12 +0700

    menyesuaikan branch homepage

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/loginView.vue'
+import HomePage from '../views/HomeView.vue'
+import RegisterView from '../views/RegisterView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'login',
+    component: LoginView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/homepage',
+    name: 'Home Page',
+    component: HomePage
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView
+  },
 ]
 
 const router = new VueRouter({
