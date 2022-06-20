2e37b34b18b623eee445915a333ca18dbed6aba9
John Doe <johndoe@email.com>
2022-06-20 19:01:22 +0700

    menyesuaikan template figma dan halaman di web

--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -4,59 +4,266 @@
       <Navbar />
     </div>
 
-    <div class="booking-select text-center mt-16">
-      <v-container fluid>
-        <v-col class="d-flex" cols="3" sm="2">
+    <div class="dropdown px-16">
+      <v-row>
+        <v-col>
           <v-select
-            :items="tipe"
+            :items="tipeRuangan"
             label="Tipe Ruangan"
-            outlined
-            background-color="#455392"
-            item-color="#455392"
+            solo
+            dark
+            background-color="#F37524"
           ></v-select>
         </v-col>
-        <v-col class="d-flex" cols="3" sm="2">
+        <v-col>
           <v-select
-            :items="jenis"
+            :items="jenisRuangan"
             label="Jenis Ruangan"
-            outlined
-            background-color="#455392"
-            item-color="#455392"
+            solo
+            dark
+            background-color="#F37524"
           ></v-select>
         </v-col>
-        <v-col class="d-flex" cols="3" sm="2">
+        <v-col>
           <v-select
-            :items="kapasitas"
-            label="Kapasitas Orang"
-            outlined
-            background-color="#455392"
-            item-color="#455392"
+            :items="kapasitasOrang"
+            label="Kapasitas Ruangan"
+            solo
+            dark
+            background-color="#F37524"
           ></v-select>
         </v-col>
-        <v-row justify="space-around">
-          <v-date-picker v-model="picker" color="#455392"></v-date-picker>
-        </v-row>
-        <h1>Pilih waktu sewa yang akan digunakan:</h1>
-        <v-row justify="space-around" align="center">
-          <v-col style="width: 350px; flex: 0 1 auto">
-            <h2>Start:</h2>
-            <v-time-picker v-model="start" :max="end"></v-time-picker>
-          </v-col>
-          <v-col style="width: 350px; flex: 0 1 auto">
-            <h2>End:</h2>
-            <v-time-picker v-model="end" :min="start"></v-time-picker>
-          </v-col>
-        </v-row>
-        <v-btn color="accent" elevation="2">Cari</v-btn>
-      </v-container>
-
-      <div class="heading-1" >Silahkan cari ruangan yang anda inginkan</div>
-      <div class="heading-3-regular mb-10">Best Seller Ruangan Meeting</div>
+        <v-col>
+          <v-btn large color="primary" block class="py-6">
+            Cari
+            <v-icon right dark> mdi-magnify </v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
     </div>
 
-    <v-footer color="#AFB2B7" class="mt-5" padless>
-      <v-col class="text-center py-16" cols="12">
-        <div class="heading-1">Copyright</div>
+    <div class="officeList mt-10 px-16">
+      <v-row>
+        <v-col>
+          <v-card class="mx-auto" max-width="280">
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="200px"
+            ></v-img>
+
+            <v-card-title> Infinite Office </v-card-title>
+
+            <v-card-text>
+              <v-row align="center" class="mx-0">
+                <v-rating
+                  :value="4.5"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+                ></v-rating>
+
+                <div class="grey--text ms-4">4.5 (413)</div>
+              </v-row>
+              <div class="my-4 text-subtitle-1">Rp. 700.000 / Jam</div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="mx-auto" max-width="280">
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="200px"
+            ></v-img>
+
+            <v-card-title> Infinite Office </v-card-title>
+
+            <v-card-text>
+              <v-row align="center" class="mx-0">
+                <v-rating
+                  :value="4.5"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+                ></v-rating>
+
+                <div class="grey--text ms-4">4.5 (413)</div>
+              </v-row>
+
+              <div class="my-4 text-subtitle-1">Rp. 700.000 / Jam</div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="mx-auto" max-width="280">
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="200px"
+            ></v-img>
+
+            <v-card-title> Infinite Office </v-card-title>
+
+            <v-card-text>
+              <v-row align="center" class="mx-0">
+                <v-rating
+                  :value="4.5"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+                ></v-rating>
+
+                <div class="grey--text ms-4">4.5 (413)</div>
+              </v-row>
+
+              <div class="my-4 text-subtitle-1">Rp. 700.000 / Jam</div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="mx-auto" max-width="280">
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="200px"
+            ></v-img>
+
+            <v-card-title> Infinite Office </v-card-title>
+
+            <v-card-text>
+              <v-row align="center" class="mx-0">
+                <v-rating
+                  :value="4.5"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+                ></v-rating>
+
+                <div class="grey--text ms-4">4.5 (413)</div>
+              </v-row>
+              <div class="my-4 text-subtitle-1">Rp. 700.000 / Jam</div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="mx-auto" max-width="280">
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="200px"
+            ></v-img>
+
+            <v-card-title> Infinite Office </v-card-title>
+
+            <v-card-text>
+              <v-row align="center" class="mx-0">
+                <v-rating
+                  :value="4.5"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+                ></v-rating>
+
+                <div class="grey--text ms-4">4.5 (413)</div>
+              </v-row>
+
+              <div class="my-4 text-subtitle-1">Rp. 700.000 / Jam</div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="mx-auto" max-width="280">
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="200px"
+            ></v-img>
+
+            <v-card-title> Infinite Office </v-card-title>
+
+            <v-card-text>
+              <v-row align="center" class="mx-0">
+                <v-rating
+                  :value="4.5"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+                ></v-rating>
+
+                <div class="grey--text ms-4">4.5 (413)</div>
+              </v-row>
+
+              <div class="my-4 text-subtitle-1">Rp. 700.000 / Jam</div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="mx-auto" max-width="280">
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="200px"
+            ></v-img>
+
+            <v-card-title> Infinite Office </v-card-title>
+
+            <v-card-text>
+              <v-row align="center" class="mx-0">
+                <v-rating
+                  :value="4.5"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+                ></v-rating>
+
+                <div class="grey--text ms-4">4.5 (413)</div>
+              </v-row>
+              <div class="my-4 text-subtitle-1">Rp. 700.000 / Jam</div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="mx-auto" max-width="280">
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="200px"
+            ></v-img>
+
+            <v-card-title> Infinite Office </v-card-title>
+
+            <v-card-text>
+              <v-row align="center" class="mx-0">
+                <v-rating
+                  :value="4.5"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+                ></v-rating>
+
+                <div class="grey--text ms-4">4.5 (413)</div>
+              </v-row>
+
+              <div class="my-4 text-subtitle-1">Rp. 700.000 / Jam</div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+    <v-footer color="#121950" class="mt-5 py-3" padless>
+      <v-col class="text-center" cols="12">
+        <div class="body-regular-2 white--text">
+          Copyright Ⓒ 2022 KOBASpace. All Right Reserved.
+        </div>
       </v-col>
     </v-footer>
   </v-app>
@@ -70,64 +277,22 @@ export default {
     Navbar,
   },
   data: () => ({
-    tipe: ["Indoor", "Outdoor"],
-    jenis: ["Event", "Meeting"],
-    kapasitas: [
-      "1-10 orang",
-      "11-20 orang",
-      "21-30 orang",
-      "31-40 orang",
-      "41-50 orang",
-      "50++ orang",
+    tipeRuangan: ["Indoor", "Outdoor"],
+    jenisRuangan: ["Event", "Meeting"],
+    kapasitasOrang: [
+      "1 - 10 Orang",
+      "11 - 20 Orang",
+      "21 - 30 Orang",
+      "31 - 40 Orang",
+      "41 - 50 Orang",
+      "50 + Orang",
     ],
-    picker: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
-      .toISOString()
-      .substr(0, 10),
-    start: null,
-    end: null,
   }),
 };
 </script>
 
 <style scoped>
-.banner-iklan {
-  margin-top: 70px;
-}
-.banner-iklan .heading-1 {
-  color: #fff !important;
-}
-.heading-1 {
-  font-family: "Nunito", sans-serif;
-  font-weight: 700 !important;
-  font-size: 32px !important;
-}
-.heading-3-regular {
-  font-family: "Nunito", sans-serif;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 20px;
-  line-height: 27px;
-  text-align: center;
-}
-.body-regular-2 {
-  font-family: "Work Sans", sans-serif;
-  font-weight: 400 !important;
-  font-size: 14px !important;
-  color: #121950 !important;
-}
-.body-regular-3 {
-  font-family: "Work Sans", sans-serif;
-  font-weight: 400 !important;
-  font-size: 12px !important;
-  color: #121950 !important;
-}
-.best-seller {
-}
-.reason-content {
-  height: 384px;
-  background-color: #afb2b7;
-}
-.testimoni {
-  background-color: #afb2b7;
+.dropdown {
+  margin-top: 90px;
 }
 </style>
