a6503c40649cdda078b495fe27030c5566283164
Mohammad Yudha Pamungkas <93898512+yudhaph0300@users.noreply.github.com>
2022-06-24 16:10:05 +0700

    update data office

--- src/views/admin/DataOffices.vue
+++ src/views/admin/DataOffices.vue
@@ -206,12 +206,18 @@
                 </v-dialog>
                 <v-dialog v-model="dialogDelete" max-width="500px">
                 <v-card>
-                    <v-card-title class="text-h6">Are you sure you want to delete this office?</v-card-title>
+                    
+                    <v-card-title>
+                      Delete Office
+                    </v-card-title>
+                    <v-card-text class="subtitle-2 mt-4">
+                      Are you sure to delete this office?
+                    </v-card-text>
                     <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                    <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-                    <v-spacer></v-spacer>
+                      <v-spacer></v-spacer>
+                      <v-btn color="primary darken-4" width="150" class="mr-4" outlined @click="closeDelete">Cancel</v-btn>
+                      <v-btn color="primary darken-4" width="150" dark  @click="deleteItemConfirm">OK</v-btn>
+                      <v-spacer></v-spacer>
                     </v-card-actions>
                 </v-card>
                 </v-dialog>
