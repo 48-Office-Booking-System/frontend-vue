c698405b804b4432e1ba30f612434e353fb0cdce
miftahasiboean <miftaheczson027@gmail.com>
2022-06-25 10:03:35 +0700

    remove some file

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import LoginView from '../views/loginView.vue'
-import HomePage from '../views/HomeView.vue'
-import RegisterView from '../views/RegisterView.vue'
 import PaymentView from '../views/PaymentView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'login',
-    component: LoginView
-  },
-  {
-    path: '/homepage',
-    name: 'Home Page',
-    component: HomePage
-  },
-  {
-    path: '/register',
-    name: 'register',
-    component: RegisterView
-  },
-  {
+    {
     path: '/paymentpage',
     name: 'payment',
     component: PaymentView
