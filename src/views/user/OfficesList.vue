ee8a551cf5b51cd6e5c9f636aea87ca65eb14296
Mohammad Yudha Pamungkas <93898512+yudhaph0300@users.noreply.github.com>
2022-06-27 12:59:54 +0700

    update offices list intregasi api

--- src/views/user/OfficesList.vue
+++ src/views/user/OfficesList.vue
@@ -1,83 +1,83 @@
 <template>
   <v-app>
     <v-app-bar
-            app height="70" 
-            class="px-10"
-            color="#FCFDFF"
-            flat
-        >
-            <div class="logo">
-                <a href="/homepage">
-                    <v-img src="../../assets/kobaspace.png" />
-                </a>
-            </div>
+        app height="70" 
+        class="px-10"
+        color="#FCFDFF"
+        flat
+    >
+        <div class="logo">
+            <a href="/homepage">
+                <v-img src="../../assets/kobaspace.png" />
+            </a>
+        </div>
 
 
-            <div class="nav mx-10">
-                <v-btn 
-                    text
-                    small
-                    color="#28304E"
-                    height="0"
-                    to="/offices"
-                >
-                    Gedung
-                </v-btn>
-                <v-btn
-                    text
-                    small
-                    color="#28304E"
-                    height="0"
-                    to="/payment"
-                >
-                    Metode Pembayaran
-                </v-btn>
-                <v-btn 
-                    text
-                    small
-                    color="#28304E"
-                    height="0"
-                    to="/aboutus"
-                >
-                    Tentang Kami
-                </v-btn>
-            </div>
+        <div class="nav mx-10">
+            <v-btn 
+                text
+                small
+                color="#28304E"
+                height="0"
+                to="/offices"
+            >
+                Gedung
+            </v-btn>
+            <v-btn
+                text
+                small
+                color="#28304E"
+                height="0"
+                to="/payment"
+            >
+                Metode Pembayaran
+            </v-btn>
+            <v-btn 
+                text
+                small
+                color="#28304E"
+                height="0"
+                to="/aboutus"
+            >
+                Tentang Kami
+            </v-btn>
+        </div>
 
-        <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
 
-            <v-text-field
-            hide-details
-            dense
-            filled
-            prepend-inner-icon="mdi-magnify"
-            placeholder="Cari gedung yang kau mau"
-            color="#28304E"
-            />
-            
+        <v-text-field
+        hide-details
+        dense
+        filled
+        prepend-inner-icon="mdi-magnify"
+        placeholder="Cari gedung yang kau mau"
+        color="#28304E"
+        />
+        
 
-            <div>
-                <v-btn
-                    class="ml-4"
-                    color="#455392"
-                    dark
-                    to="/"
-                >
-                    Masuk
-                </v-btn>
+        <div>
+            <v-btn
+                class="ml-4"
+                color="#455392"
+                dark
+                to="/"
+            >
+                Masuk
+            </v-btn>
 
-                <v-btn
-                    class="ml-4"
-                    outlined
-                    color="#455392"
-                    to="/register"
-                >
-                    Daftar
-                </v-btn>
-            </div>
+            <v-btn
+                class="ml-4"
+                outlined
+                color="#455392"
+                to="/register"
+            >
+                Daftar
+            </v-btn>
+        </div>
 
-        </v-app-bar>
+    </v-app-bar>
 
-    <div class="dropdown px-16">
+    <!-- <div class="dropdown px-16">
       <v-row>
         <v-col>
           <v-select
@@ -109,27 +109,69 @@
         <v-col>
           <v-btn large color="#121950" block class="py-6" solo dark>
             Cari
-            <v-icon right dark> mdi-magnify </v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </div> -->
+
+    <v-card
+     color="primary darken-4"
+    >
+      
+      <div class="filter px-16 pt-6">
+        
+      <v-row>
+        <v-col>
+          <v-select
+            :items="tipeRuangan"
+            label="Tipe Ruangan"
+            solo
+            dark
+            background-color="#F37524"
+          ></v-select>
+        </v-col>
+        <v-col>
+          <v-select
+            :items="jenisRuangan"
+            label="Jenis Ruangan"
+            solo
+            dark
+            background-color="#F37524"
+          ></v-select>
+        </v-col>
+        <v-col>
+          <v-select
+            :items="kapasitasOrang"
+            label="Kapasitas Ruangan"
+            solo
+            dark
+            background-color="#F37524"
+          ></v-select>
+        </v-col>
+        <v-col>
+          <v-btn large color="primary" block class="py-6" solo dark>
+            Cari
           </v-btn>
         </v-col>
       </v-row>
     </div>
+    </v-card>
 
     <div class="officeList mt-10 px-16">
       <v-row>
-        <v-col>
+        <v-col v-for="office in offices" :key="office.id">
           <v-card class="mx-auto" max-width="280">
             <v-img
-              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              :src="office.photo[0]"
               height="200px"
             ></v-img>
 
-            <v-card-title> Infinite Office </v-card-title>
+            <v-card-title> {{ office.name }} </v-card-title>
 
             <v-card-text>
               <v-row align="center" class="mx-0">
                 <v-rating
-                  :value="4.5"
+                  :value="4"
                   color="amber"
                   dense
                   half-increments
@@ -137,201 +179,27 @@
                   size="14"
                 ></v-rating>
 
-                <div class="grey--text ms-4">4.5 (413)</div>
+                <div class="grey--text ms-4">4</div>
               </v-row>
-              <div class="my-4 text-subtitle-1">Rp. 700.000 / Jam</div>
+              <div class="mt-4 text-subtitle-1">Harga : Rp. {{ office.price }} / Jam</div>
             </v-card-text>
+
+            <v-divider class="mx-2"></v-divider>
+            <v-card-actions>
+              <v-btn
+               block
+               large
+               color="primary"
+              >
+                Lihat Detail
+              </v-btn>
+            </v-card-actions>
           </v-card>
         </v-col>
-        <v-col>
-          <v-card class="mx-auto" max-width="280">
-            <v-img
-              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-              height="200px"
-            ></v-img>
 
-            <v-card-title> Infinite Office </v-card-title>
-
-            <v-card-text>
-              <v-row align="center" class="mx-0">
-                <v-rating
-                  :value="4.5"
-                  color="amber"
-                  dense
-                  half-increments
-                  readonly
-                  size="14"
-                ></v-rating>
-
-                <div class="grey--text ms-4">4.5 (413)</div>
-              </v-row>
-
-              <div class="my-4 text-subtitle-1">Rp. 700.000 / Jam</div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card class="mx-auto" max-width="280">
-            <v-img
-              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-              height="200px"
-            ></v-img>
-
-            <v-card-title> Infinite Office </v-card-title>
-
-            <v-card-text>
-              <v-row align="center" class="mx-0">
-                <v-rating
-                  :value="4.5"
-                  color="amber"
-                  dense
-                  half-increments
-                  readonly
-                  size="14"
-                ></v-rating>
-
-                <div class="grey--text ms-4">4.5 (413)</div>
-              </v-row>
-
-              <div class="my-4 text-subtitle-1">Rp. 700.000 / Jam</div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card class="mx-auto" max-width="280">
-            <v-img
-              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-              height="200px"
-            ></v-img>
-
-            <v-card-title> Infinite Office </v-card-title>
-
-            <v-card-text>
-              <v-row align="center" class="mx-0">
-                <v-rating
-                  :value="4.5"
-                  color="amber"
-                  dense
-                  half-increments
-                  readonly
-                  size="14"
-                ></v-rating>
-
-                <div class="grey--text ms-4">4.5 (413)</div>
-              </v-row>
-              <div class="my-4 text-subtitle-1">Rp. 700.000 / Jam</div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card class="mx-auto" max-width="280">
-            <v-img
-              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-              height="200px"
-            ></v-img>
-
-            <v-card-title> Infinite Office </v-card-title>
-
-            <v-card-text>
-              <v-row align="center" class="mx-0">
-                <v-rating
-                  :value="4.5"
-                  color="amber"
-                  dense
-                  half-increments
-                  readonly
-                  size="14"
-                ></v-rating>
-
-                <div class="grey--text ms-4">4.5 (413)</div>
-              </v-row>
-
-              <div class="my-4 text-subtitle-1">Rp. 700.000 / Jam</div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card class="mx-auto" max-width="280">
-            <v-img
-              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-              height="200px"
-            ></v-img>
-
-            <v-card-title> Infinite Office </v-card-title>
-
-            <v-card-text>
-              <v-row align="center" class="mx-0">
-                <v-rating
-                  :value="4.5"
-                  color="amber"
-                  dense
-                  half-increments
-                  readonly
-                  size="14"
-                ></v-rating>
-
-                <div class="grey--text ms-4">4.5 (413)</div>
-              </v-row>
-
-              <div class="my-4 text-subtitle-1">Rp. 700.000 / Jam</div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card class="mx-auto" max-width="280">
-            <v-img
-              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-              height="200px"
-            ></v-img>
-
-            <v-card-title> Infinite Office </v-card-title>
-
-            <v-card-text>
-              <v-row align="center" class="mx-0">
-                <v-rating
-                  :value="4.5"
-                  color="amber"
-                  dense
-                  half-increments
-                  readonly
-                  size="14"
-                ></v-rating>
-
-                <div class="grey--text ms-4">4.5 (413)</div>
-              </v-row>
-              <div class="my-4 text-subtitle-1">Rp. 700.000 / Jam</div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card class="mx-auto" max-width="280">
-            <v-img
-              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-              height="200px"
-            ></v-img>
-
-            <v-card-title> Infinite Office </v-card-title>
-
-            <v-card-text>
-              <v-row align="center" class="mx-0">
-                <v-rating
-                  :value="4.5"
-                  color="amber"
-                  dense
-                  half-increments
-                  readonly
-                  size="14"
-                ></v-rating>
-
-                <div class="grey--text ms-4">4.5 (413)</div>
-              </v-row>
-
-              <div class="my-4 text-subtitle-1">Rp. 700.000 / Jam</div>
-            </v-card-text>
-          </v-card>
-        </v-col>
       </v-row>
     </div>
+
     <v-footer color="#121950" class="mt-5 py-3" padless>
       <v-col class="text-center" cols="12">
         <div class="body-regular-2 white--text">
@@ -343,6 +211,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: "BookingPage",
   data: () => ({
@@ -356,12 +225,30 @@ export default {
       "41 - 50 Orang",
       "50 + Orang",
     ],
+    offices: [],
+
+
+
   }),
-};
+  async mounted() {
+    this.initialize()
+  },
+  methods: {
+    async loadDataOffices() {
+      const response = await axios.get(`http://localhost:3000/offices`)
+      this.offices = response.data
+    },
+    initialize() {
+      this.loadDataOffices()
+    }
+  }
+
+
+}
 </script>
 
 <style scoped>
-.dropdown {
-  margin-top: 90px;
+.filter {
+  margin-top: 70px;
 }
 </style>
